ec8907383893e13634de23fa0fd32c10b5471831
pocky1559 <pocky1559@proton.me>
2025-12-10 19:42:58 +0700

    update the language to Thai and add supabase

---
+++ scripts/login/login.js
@@ -0,0 +1,7 @@
+// Login function
+document.getElementById('studentForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const studentId = document.getElementById('studentIdOrEmail').value;
+    const password = document.getElementById('studentPassword').value;
+});
---
+++ scripts/supabase-cilent.js
@@ -0,0 +1,5 @@
+import { createClient } from "@supabase/supabase-js";   
+
+const supabaseUrl = "https://lkcsqeqzpteevlpazhqo.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxrY3NxZXF6cHRlZXZscGF6aHFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzNjMyNTksImV4cCI6MjA4MDkzOTI1OX0.NqmuGydeXEV-VB5c9cVdNkymtg07Llt61VQ3pzFdARM";
+export const supabase = createClient(supabaseUrl, supabaseKey);

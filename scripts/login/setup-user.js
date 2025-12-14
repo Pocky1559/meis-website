deb784e4ff8af836c302c6d67024085f2be20e20
pocky1559 <pocky1559@proton.me>
2025-12-14 22:07:08 +0700

    add user setup form with validation and styling

--- scripts/login/setup-user.js
+++ scripts/login/setup-user.js
@@ -1,1 +1,17 @@
 import { supabase } from '../supabase-cilent.js';
+
+document.getElementById('studentForm').addEventListener('submit', async function(e) {
+    e.preventDefault();
+    
+    const studentId = document.getElementById('studentId').value;
+    const studentFirstName = document.getElementById('studentFirstName').value;
+    const studentLastName = document.getElementById('studentLastName').value;
+    const studentClass = document.getElementById('studentClass').value;
+    const studentNumber = document.getElementById('studentNumber').value;
+
+    console.log('Student ID:', studentId);
+    console.log('Student First Name:', studentFirstName);
+    console.log('Student Last Name:', studentLastName);
+    console.log('Student Class:', studentClass);
+    console.log('Student Number:', studentNumber);
+});
